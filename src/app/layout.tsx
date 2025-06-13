b47a66a4079623cd758808a5f7bf7bdfe251d6ce
devusmonjon <usmonjonhasanov777@gmail.com>
2025-06-13 14:56:52 +0500

    final fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Grandpharm Templates",
   description: "Grandpharm sms Templates",
-  manifest: "./manifest.json",
+  manifest: "/manifest.json",
 };
 
 export function generateStaticParams() {
